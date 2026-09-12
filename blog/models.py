f0bb03dfda8c404adc25803d22d1a4e5ed18482d
nurbektech <nurbektech@gmail.com>
2026-09-12 17:05:06 +0500

    feat: add html templates

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created"]
+
+    def __str__(self):
+        return self.title
+    
